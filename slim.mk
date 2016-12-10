b17211e723f3ede1524eb569879adc56766fe27b
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2016-12-10 15:11:40 +0100

    Slim-Ify
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

---
+++ slim.mk
@@ -0,0 +1,17 @@
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit common aosp telephony
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Release name
+PRODUCT_RELEASE_NAME := LG L90
+PRODUCT_NAME := slim_w7
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := w7
+
+$(call inherit-product, device/lge/w7/full_w7.mk)
